c8d1d59ce47e8f96027d342ab6419b96416f9733
Fhraze <bcfrazao@gmail.com>
2025-05-23 20:15:01 -0300

    yipee

---
+++ install-packages.sh
@@ -0,0 +1,8 @@
+sudo pacman -Syu
+
+# Essential
+sudo pacman -S malcontent grim slurp wl-clipboard noto-fonts-emoji hyprland swaybg grim slurp hyprland xdg-desktop-portal xdg-desktop-portal-hyprland wofi waybar thunar thunar-volman wine --needed
+
+# Extra
+sudo pacman -S neovim keepassxc github-cli mpv winetricks steam krita jdk-openjdk mono qbittorrent yt-dlp lolcat cmatrix cava libreoffice rustup syncthing --needed
+
---
+++ setup-oculus.sh
@@ -0,0 +1,17 @@
+sudo pacman -Syu
+sudo pacman -S openvr openxr corectrl unzip tar android-tools --needed --noconfirm
+sudo mkdir -p /etc/udev/rules.d
+if grep -q "SUBSYSTEM" /etc/udev/rules.d/50-oculus.rules; then
+  echo "Rules already applied"
+else
+  echo "SUBSYSTEM="usb", ATTR{idVendor}=="2833", ATTR{idProduct}=="0186", MODE="0660" group="plugdev", symlink+="ocuquest%n"" | sudo tee -a /etc/udev/rules.d/50-oculus.rules
+fi
+
+# Start ALVR Setup
+#mkdir ~/ALVR
+#cd ~/ALVR
+
+# Download ADB Forwarder for ALVR
+#wget https://github.com/alvr-org/ADBForwarder/releases/download/v1.3.1/ADBForwarder-linux-x64.zip
+#unzip ADBForwarder-linux-x64.zip
+#rm ADBForwarder-linux-x64.zip
