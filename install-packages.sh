22aeb7e9da6ecf148c3b48863bb195d6a7259c43
Fhraze <bcfrazao@gmail.com>
2025-06-13 12:42:37 -0300

    added gamemode

--- install-packages.sh
+++ install-packages.sh
@@ -1,7 +1,7 @@
 sudo pacman -Syu
 
 # Essential
-sudo pacman -S noto-fonts-cjk jq malcontent grim slurp wl-clipboard noto-fonts-emoji hyprland swaybg grim slurp hyprland xdg-desktop-portal xdg-desktop-portal-hyprland wofi waybar thunar thunar-volman wine --needed
+sudo pacman -S gamemode noto-fonts-cjk jq malcontent grim slurp wl-clipboard noto-fonts-emoji hyprland swaybg grim slurp hyprland xdg-desktop-portal xdg-desktop-portal-hyprland wofi waybar thunar thunar-volman wine --needed
 
 # Extra
 sudo pacman -S neovim keepassxc github-cli mpv winetricks steam krita jdk-openjdk mono qbittorrent yt-dlp lolcat cmatrix cava libreoffice rustup syncthing --needed
